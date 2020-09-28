671862db98a86c45500fae15d441102e1d9b4ccc
petro <gapierre4@gmail.com>
2020-09-28 10:37:36 +0300

    work

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("This is my laba 3");
+            int x = 7;
+            int y = x + 5;
+
+        }
+    }
+}
