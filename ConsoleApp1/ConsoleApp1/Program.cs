f5338d4fe95708cf15c22218f2bdeb3c21607072
petro <gapierre4@gmail.com>
2020-09-30 19:57:17 +0300

    commit on master

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,14 +8,10 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.Write("This is my laba 3");
-            int x = 9;
-<<<<<<< HEAD
+            int x = 6;
             int y = x + 5;
             int z = y + 2;
-=======
-            int y = x + 1;
->>>>>>> 57f9f8aec32cbcd508c2b1debcb7e6e599e0b750
-
+            int a = z + 4;
         }
     }
 }
