0958645108e267d606a5df1bd235e6beefe54b97
petro <gapierre4@gmail.com>
2020-10-01 09:01:04 +0300

    Merge branch 'master' into br2
    # Conflicts:
    #	ConsoleApp1/ConsoleApp1/Program.cs
    #	ConsoleApp1/ConsoleApp1/obj/Debug/netcoreapp3.1/ConsoleApp1.csprojAssemblyReference.cache

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,10 +8,11 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.Write("This is my laba 3");
-            int x = 2;
+            int x = 7;
             int y = x + 5;
             int l = y + 4;
             int k = l - 2;
+            int v = l + 1;
         }
     }
 }
