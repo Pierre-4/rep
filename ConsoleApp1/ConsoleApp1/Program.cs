b4b4475912f4c210a3368ac9b87474ee5daee25a
petro <gapierre4@gmail.com>
2020-10-01 09:02:36 +0300

    comm1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,10 +8,11 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.Write("This is my laba 3");
-            int x = 6;
+            int x = 1;
             int y = x + 5;
             int z = y + 2;
-            int a = z + 4;
+            int b = z + 4;
+
         }
     }
 }
